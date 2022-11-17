7387c3a00cd656736dd2b83099de21aaee5e719a
smessie <smessie@smessie.com>
2022-11-17 23:17:07 +0100

    feat: Add authentication

--- src/components/EyeReasoner.vue
+++ src/components/EyeReasoner.vue
@@ -3,13 +3,39 @@
     <h1>Eye Reasoner</h1>
     <MDBCard>
       <MDBCardBody class="w-100">
+        <MDBCardTitle>Authentication</MDBCardTitle>
+        <MDBCardText>
+          <div v-if="loggedIn">
+            <span class="text-success">
+              You are logged in as <em>{{ loggedIn }}</em
+              >.
+            </span>
+          </div>
+          <div v-else>
+            <MDBInput
+              v-model="oidcIssuer"
+              label="OIDC Issuer"
+              type="text"
+              required
+              style="margin-bottom: 1rem"
+            />
+            <MDBBtn color="primary" @click="login">Login</MDBBtn>
+          </div>
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+    <MDBCard>
+      <MDBCardBody class="w-100">
         <MDBCardTitle>Input: N3 Document</MDBCardTitle>
         <MDBCardText>
-          <MDBSwitch
-            v-model="isUrl"
-            label="Via URL"
-            labelColor="primary"
-          ></MDBSwitch>
+          <div style="margin-bottom: 1rem">
+            <MDBSwitch
+              v-model="isUrl"
+              label="Via URL"
+              labelColor="primary"
+              style="margin-bottom: 3rem !important"
+            ></MDBSwitch>
+          </div>
           <MDBInput
             label="Dataset URL"
             type="url"
@@ -61,6 +87,7 @@ import {
   MDBSwitch,
   MDBTextarea,
 } from "mdb-vue-ui-kit";
+import {getDefaultSession, handleIncomingRedirect, login, fetch} from "@inrupt/solid-client-authn-browser";
 
 export default {
   name: "EyeReasoner",
@@ -88,7 +115,16 @@ export default {
       n3queryUrl: "",
       output: "",
       isUrl: true,
+      loggedIn: undefined,
+      oidcIssuer: "",
     };
+  },
+  created() {
+    handleIncomingRedirect({
+      restorePreviousSession: true,
+    }).then((info) => {
+      this.loggedIn = info.webId;
+    });
   },
   methods: {
     async execute(event) {
@@ -153,6 +189,23 @@ export default {
         this.output = json.error;
       }
       console.log(json.success);
+    },
+    async login() {
+      await handleIncomingRedirect();
+
+      // 2. Start the Login Process if not already logged in.
+      if (!getDefaultSession().info.isLoggedIn) {
+        await login({
+          // Specify the URL of the user's Solid Identity Provider;
+          // e.g., "https://login.inrupt.com".
+          oidcIssuer: this.oidcIssuer,
+          // Specify the URL the Solid Identity Provider should redirect the user once logged in,
+          // e.g., the current page for a single-page app.
+          redirectUrl: window.location.href,
+          // Provide a name for the application when sending to the Solid Identity Provider
+          clientName: "SWIPL-WASM-Example",
+        });
+      }
     },
   },
 };
